55da1e6305154c5b930fc599858ac1983aac61ba
Manokl <69851478+Manokl@users.noreply.github.com>
2020-08-20 13:13:43 +0300

    Create Main.java

---
+++ task_1.5/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		double TC = scan.nextDouble();
+		double a = 9;
+		double b = 5;
+		System.out.println((a/b) * TC + 32);
+	}
+}
